aead3df18354a0efd62aa655392bc3a893409f3c
bluxa <81200014+bluxa@users.noreply.github.com>
2021-10-11 20:44:22 -0600

    Creación de algoritmo

--- AlgoritmoDelBanquero/Form1.Designer.cs
+++ AlgoritmoDelBanquero/Form1.Designer.cs
@@ -28,13 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNoProcesos = new System.Windows.Forms.TextBox();
+            this.txtNoRecursos = new System.Windows.Forms.TextBox();
+            this.btnCrearArray = new System.Windows.Forms.Button();
+            this.dgvAsignados = new System.Windows.Forms.DataGridView();
+            this.dgvMaximos = new System.Windows.Forms.DataGridView();
+            this.btnEjecutar = new System.Windows.Forms.Button();
+            this.dgvDisponibles = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAsignados)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMaximos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDisponibles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(82, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese no. de los procesos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(82, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingrese no. de los recursos";
+            // 
+            // txtNoProcesos
+            // 
+            this.txtNoProcesos.Location = new System.Drawing.Point(325, 35);
+            this.txtNoProcesos.Name = "txtNoProcesos";
+            this.txtNoProcesos.Size = new System.Drawing.Size(100, 20);
+            this.txtNoProcesos.TabIndex = 2;
+            // 
+            // txtNoRecursos
+            // 
+            this.txtNoRecursos.Location = new System.Drawing.Point(325, 92);
+            this.txtNoRecursos.Name = "txtNoRecursos";
+            this.txtNoRecursos.Size = new System.Drawing.Size(100, 20);
+            this.txtNoRecursos.TabIndex = 3;
+            // 
+            // btnCrearArray
+            // 
+            this.btnCrearArray.Location = new System.Drawing.Point(484, 59);
+            this.btnCrearArray.Name = "btnCrearArray";
+            this.btnCrearArray.Size = new System.Drawing.Size(75, 23);
+            this.btnCrearArray.TabIndex = 4;
+            this.btnCrearArray.Text = "Aceptar";
+            this.btnCrearArray.UseVisualStyleBackColor = true;
+            this.btnCrearArray.Click += new System.EventHandler(this.btnCrearArray_Click);
+            // 
+            // dgvAsignados
+            // 
+            this.dgvAsignados.AllowUserToAddRows = false;
+            this.dgvAsignados.AllowUserToDeleteRows = false;
+            this.dgvAsignados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAsignados.Location = new System.Drawing.Point(41, 166);
+            this.dgvAsignados.Name = "dgvAsignados";
+            this.dgvAsignados.Size = new System.Drawing.Size(542, 150);
+            this.dgvAsignados.TabIndex = 5;
+            // 
+            // dgvMaximos
+            // 
+            this.dgvMaximos.AllowUserToAddRows = false;
+            this.dgvMaximos.AllowUserToDeleteRows = false;
+            this.dgvMaximos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMaximos.Location = new System.Drawing.Point(41, 342);
+            this.dgvMaximos.Name = "dgvMaximos";
+            this.dgvMaximos.Size = new System.Drawing.Size(542, 150);
+            this.dgvMaximos.TabIndex = 6;
+            // 
+            // btnEjecutar
+            // 
+            this.btnEjecutar.Location = new System.Drawing.Point(608, 265);
+            this.btnEjecutar.Name = "btnEjecutar";
+            this.btnEjecutar.Size = new System.Drawing.Size(75, 23);
+            this.btnEjecutar.TabIndex = 7;
+            this.btnEjecutar.Text = "Ejecutar";
+            this.btnEjecutar.UseVisualStyleBackColor = true;
+            this.btnEjecutar.Click += new System.EventHandler(this.btnEjecutar_Click);
+            // 
+            // dgvDisponibles
+            // 
+            this.dgvDisponibles.AllowUserToAddRows = false;
+            this.dgvDisponibles.AllowUserToDeleteRows = false;
+            this.dgvDisponibles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDisponibles.Location = new System.Drawing.Point(41, 523);
+            this.dgvDisponibles.Name = "dgvDisponibles";
+            this.dgvDisponibles.Size = new System.Drawing.Size(542, 150);
+            this.dgvDisponibles.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(721, 450);
+            this.Controls.Add(this.dgvDisponibles);
+            this.Controls.Add(this.btnEjecutar);
+            this.Controls.Add(this.dgvMaximos);
+            this.Controls.Add(this.dgvAsignados);
+            this.Controls.Add(this.btnCrearArray);
+            this.Controls.Add(this.txtNoRecursos);
+            this.Controls.Add(this.txtNoProcesos);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAsignados)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMaximos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDisponibles)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNoProcesos;
+        private System.Windows.Forms.TextBox txtNoRecursos;
+        private System.Windows.Forms.Button btnCrearArray;
+        private System.Windows.Forms.DataGridView dgvAsignados;
+        private System.Windows.Forms.DataGridView dgvMaximos;
+        private System.Windows.Forms.Button btnEjecutar;
+        private System.Windows.Forms.DataGridView dgvDisponibles;
     }
 }
 
--- AlgoritmoDelBanquero/Form1.cs
+++ AlgoritmoDelBanquero/Form1.cs
@@ -12,9 +12,151 @@ namespace AlgoritmoDelBanquero
 {
     public partial class Form1 : Form
     {
+        private int[,] necesarios;
+        private int[,] asignados;
+        private int[,] maximos;
+        private int[,] disponibles;
+        private int numeroProcesos;
+        private int numeroRecursos;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void btnCrearArray_Click(object sender, EventArgs e)
+        {
+            numeroRecursos = Convert.ToInt32(txtNoRecursos.Text);
+            numeroProcesos = Convert.ToInt32(txtNoProcesos.Text);
+
+            for (int i = 0; i < numeroRecursos; i++)
+            {
+                dgvAsignados.Columns.Add("" + i, "" + i);
+                dgvMaximos.Columns.Add("" + i, "" + i);
+                dgvDisponibles.Columns.Add("" + i, "" + i);
+            }
+
+            for (int i = 0; i < numeroProcesos; i++)
+            {
+                dgvAsignados.Rows.Add("");
+                dgvMaximos.Rows.Add("");
+            }
+
+            for (int i = 0; i < 1; i++)
+            {
+                dgvDisponibles.Rows.Add("");
+            }
+        }
+
+        private void btnEjecutar_Click(object sender, EventArgs e)
+        {
+            asignados = new int[dgvAsignados.Rows.Count, dgvAsignados.Columns.Count];
+
+            foreach (DataGridViewRow row in dgvAsignados.Rows)
+            {
+                if (!row.IsNewRow)
+                {
+                    foreach (DataGridViewCell cel in row.Cells)
+                    {
+                        asignados[cel.RowIndex, cel.ColumnIndex] = Convert.ToInt32(cel.Value);
+                    }
+                }
+            }
+
+            maximos = new int[dgvMaximos.Rows.Count, dgvMaximos.Columns.Count];
+
+            foreach (DataGridViewRow row in dgvMaximos.Rows)
+            {
+                if (!row.IsNewRow)
+                {
+                    foreach (DataGridViewCell cel in row.Cells)
+                    {
+                        maximos[cel.RowIndex, cel.ColumnIndex] = Convert.ToInt32(cel.Value);
+                    }
+                }
+            }
+
+            disponibles = new int[dgvDisponibles.Rows.Count, dgvDisponibles.Columns.Count];
+
+            foreach (DataGridViewRow row in dgvDisponibles.Rows)
+            {
+                if (!row.IsNewRow)
+                {
+                    foreach (DataGridViewCell cel in row.Cells)
+                    {
+                        //disponibles[cel.RowIndex, cel.ColumnIndex] = Convert.ToInt32(cel.Value);
+                        disponibles[0, cel.ColumnIndex] = Convert.ToInt32(cel.Value);
+                    }
+                }
+            }
+            necesarios = new int[numeroProcesos, numeroRecursos];
+            calculoNecesarios();
+
+            Boolean[] done = new Boolean[numeroProcesos];
+            int j = 0;
+
+            while (j < numeroProcesos)
+            {  //hasta que todos los procesos se asignen
+                Boolean asignado = false;
+                for (int i = 0; i < numeroProcesos; i++)
+                {
+                    if (!done[i] && chequear(i))
+                    {  //intentando asignar
+                        for (int k = 0; k < numeroRecursos; k++)
+                        {
+                            disponibles[0, k] = disponibles[0, k] - necesarios[i, k] + maximos[i, k];
+                        }
+                        MessageBox.Show("Proceso asignado : " + i);
+                        //Console.WriteLine("Proceso asignado : " + i);
+                        asignado = done[i] = true;
+                        j++;
+                    }
+                }
+                if (!asignado)
+                {
+                    break;  //si no esta asignado
+                }
+            }
+            if (j == numeroProcesos) //si todos los procesos estan asignados
+            {
+                MessageBox.Show("\nAsignado de forma segura");
+                //Console.WriteLine("\nAsignado de forma segura");
+            }
+            else
+            {
+                MessageBox.Show("Todos los procesos se pueden asignar de forma segura");
+                //Console.WriteLine("Todos los procesos se pueden asignar de forma segura");
+            }
+        }
+
+
+        private int[,] calculoNecesarios()
+        {
+            for (int i = 0; i < numeroProcesos; i++)
+            {
+                for (int j = 0; j < numeroRecursos; j++) //calculando matriz de necesarios
+                {
+                    necesarios[i, j] = maximos[i, j] - asignados[i, j];
+                }
+            }
+
+            return necesarios;
+        }
+
+        private Boolean chequear(int i)
+        {
+            //chequeando si todos los recursos para el proceso pueden ser asignados
+            for (int j = 0; j < numeroRecursos; j++)
+            {
+                if (disponibles[0, j] < necesarios[i, j])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+
     }
 }
